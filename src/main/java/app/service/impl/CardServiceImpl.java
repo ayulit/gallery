44c81a37cbc54d2c13ac4f1e3b52fe7c49d11aa0
alitvin <litwinoff@gmail.com>
2020-07-06 23:19:59 +0300

    Change to @Transactional by Spring.

--- src/main/java/app/service/impl/CardServiceImpl.java
+++ src/main/java/app/service/impl/CardServiceImpl.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
--- src/main/java/app/service/impl/PersonServiceImpl.java
+++ src/main/java/app/service/impl/PersonServiceImpl.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
